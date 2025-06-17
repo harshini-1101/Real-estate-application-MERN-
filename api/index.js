c79ef3ec0a40357af433ff8c527ce29ea7b9aa12
harshini-1101 <suharshini1101@gmail.com>
2025-06-18 02:13:26 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("mongodb connected successfully");
+}).catch((e)=>{
+    console.log(e);
+});
 
 const app = express();
 
