79027930bb1276b32ff8ed461e1633a4872f726c
harshini-1101 <suharshini1101@gmail.com>
2025-07-02 00:06:39 +0530

    created sign in api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,5 +1,7 @@
 const bcryptjs = require("bcryptjs");
 const User = require("../models/User.model");
+const errorHandler = require("../utils/error");
+const jwt = require('jsonwebtoken');
 
 const signup = async(req,res,next)=>{
     const {username, email, password} = req.body;
@@ -12,7 +14,21 @@ const signup = async(req,res,next)=>{
     catch(err){
         next(err);
     }
-    
 }
 
-module.exports = signup;
+const signin = async(req,res,next)=>{
+    const {email, password} = req.body;
+    try {
+        validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404,'User not found'));
+        validPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword) return next(errorHandler(401,'invalid password'));
+        const token = jwt.sign({id : validUser._id},process.env.JWT_SECRET);
+        const {password: pass, ...rest} = validUser._doc;
+        res.cookie('access_token',token, {httpOnly:true}).status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = {signup, signin};
